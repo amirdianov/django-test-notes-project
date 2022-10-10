861019abd31fbaee64bb440c52435452113cdfc4
amirdianov <amirdianov@gmail.com>
2022-10-11 01:44:29 +0300

    done project

--- web/views.py
+++ web/views.py
@@ -4,14 +4,10 @@ from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, RedirectView, FormView, CreateView, UpdateView, DeleteView
 
 from web.forms import NoteForm, AuthForm
 from web.models import Note, Tag, User
-
-
-def main_view(request):
-    return redirect("notes_list")
 
 
 class NotesListView(ListView):
@@ -72,9 +68,15 @@ class NoteDetailView(DetailView):
         return Note.objects.filter(user=self.request.user)
 
 
-class NoteCreateFormView(CreateView):
-    form_class = NoteForm
+class NoteMixin:
     template_name = 'web/note_form.html'
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Note.objects.none()
+        return Note.objects.filter(user=self.request.user)
 
     def get_initial(self):
         return {'user': self.request.user}
@@ -83,26 +85,26 @@ class NoteCreateFormView(CreateView):
         return reverse('note', args=(self.object.title, self.object.id))
 
 
-@login_required
-def note_edit_view(request, id=None, title=None):
-    form = NoteForm()
+class NoteCreateFormView(NoteMixin, CreateView):
+    form_class = NoteForm
 
-    note = None
-    if id is not None:
-        note = get_object_or_404(Note, user=request.user, id=id)
-        form = NoteForm(instance=note)
 
-    if request.method == 'POST':
-        form = NoteForm(request.POST, instance=note, initial={'user': request.user})
-        if form.is_valid():
-            note = form.save()
-            return redirect('note', note.title, note.id)
+class NoteUpdateView(NoteMixin, UpdateView):
+    form_class = NoteForm
 
-    return render(request, "web/note_form.html", {
-        'id': id,
-        'form': form,
-        'title': getattr(note, 'title')
-    })
+    def get_context_data(self, **kwargs):
+        return {
+            **super(NoteUpdateView, self).get_context_data(**kwargs),
+            'id': self.kwargs[self.slug_url_kwarg],
+            'title': self.object.title
+        }
+
+
+class NoteDeleteView(NoteMixin, DeleteView):
+    template_name = 'web/note_delete.html'
+
+    def get_success_url(self):
+        return reverse('main')
 
 
 class RegistrationView(View):
