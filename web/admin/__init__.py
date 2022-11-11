fbae75745daa6ece123fa402079c5a464619babd
diyanovamir <amirdianov@gmail.com>
2022-11-11 13:23:36 +0300

    +admin domains filter done +user admin to admin panel

--- web/admin/__init__.py
+++ web/admin/__init__.py
@@ -3,7 +3,9 @@ from django.contrib import admin, messages
 # Register your models here.
 from web.admin.note import NoteAdmin
 from web.admin.tag import TagAdmin
-from web.models import Note, Tag, NoteComment
+from web.models import Note, Tag, NoteComment, User
+from web.admin.user import UserAdmin
 
 admin.site.register(Note, NoteAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(User, UserAdmin)
--- web/admin/note.py
+++ web/admin/note.py
@@ -1,6 +1,10 @@
 from django.contrib import admin, messages
+from django.db.models import F, Func
+from django.db.models.expressions import RawSQL
+from web.db_utils import SplitPartFunc
+from web.models import Note, NoteComment, User
 
-from web.models import Note, NoteComment
+from web.models import Note, NoteComment, User
 
 
 @admin.display(description='Привести название к верхнему регистру')
@@ -22,11 +26,34 @@ class NoteCommentInline(admin.TabularInline):
     model = NoteComment
 
 
+class AuthorEmailDomainFilter(admin.SimpleListFilter):
+    title = 'Домен автора заметки'
+    parameter_name = 'author_email_domain'
+
+    def has_output(self):
+        return True
+
+    def lookups(self, request, model_admin):
+        email_domains = (
+            User.objects.all()
+            .annotate(email_domain=SplitPartFunc("email", "@", 2))
+            .distinct()
+            .values_list("email_domain", flat=True)
+        )
+        print(email_domains.query)
+        return ((d, d) for d in email_domains)
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(user__email__endswith=f'@{self.value()}')
+        return queryset
+
+
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'user', 'get_text_count', 'created_at')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'text')
-    list_filter = ('created_at',)
+    list_filter = ('created_at', 'tags', AuthorEmailDomainFilter)
     ordering = ('created_at',)
     readonly_fields = ('alert_send_at', 'get_text_count')
     actions = (set_title_to_uppercase,)
