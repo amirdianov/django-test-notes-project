12e77ab2229ea910eddcf2b60c26e67916a88f03
diyanovamir <amirdianov@gmail.com>
2022-11-11 13:24:27 +0300

    +start middleware

--- notes/settings.py
+++ notes/settings.py
@@ -46,6 +46,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "notes.debug.SqlPrintingMiddleware",
+
 ]
 
 ROOT_URLCONF = 'notes.urls'
