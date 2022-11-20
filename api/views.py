63c35f7dd76a9329273e98cf6327fe43867e3482
diyanovamir <amirdianov@gmail.com>
2022-11-20 18:37:14 +0300

    + modelviewset

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework import status, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from api.serializers import NoteSerializer
 from web.models import Note, NoteComment
@@ -14,9 +14,7 @@ def status_view(request):
     return Response({"status": "ok"})
 
 
-class NoteViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin,
-                  mixins.RetrieveModelMixin,
-                  GenericViewSet):
+class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all().optimize_for_lists().prefetch_related(
         Prefetch('comments', NoteComment.objects.all().order_by("created_at"))
     )
