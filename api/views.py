343bb337613e249df018aaa43f0030a75062b318
diyanovamir <amirdianov@gmail.com>
2022-11-20 14:06:51 +0300

    +note view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -14,4 +15,11 @@ def status_view(request):
 def notes_view(request):
     notes = Note.objects.all()
     serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def note_view(request, id):
+    note = get_object_or_404(Note, id=id)
+    serializer = NoteSerializer(note)
     return Response(serializer.data)
