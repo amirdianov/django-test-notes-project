8037d626f394c41208dabd8f03e79ba798622a9a
diyanovamir <amirdianov@gmail.com>
2022-12-09 01:14:41 +0300

    + pre commit in dockerfile

--- makemigrations/apps.py
+++ makemigrations/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MakemigrationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'makemigrations'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "makemigrations"
