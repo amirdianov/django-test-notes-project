9c4612cab0507e3d719139c0d9138c1cd11b946c
diyanovamir <amirdianov@gmail.com>
2023-04-08 00:41:34 +0300

    + display user in header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,10 @@
 <script setup>
 import {RouterView} from 'vue-router'
+import MainHeader from "@/containers/MainHeader.vue";
 </script>
 
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-container>
-            <b-navbar-brand to="/">Notes</b-navbar-brand>
-
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item :to="{name: 'home'}">Home</b-nav-item>
-                    <b-nav-item :to="{name: 'login'}">Вход</b-nav-item>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-container>
-    </b-navbar>
+    <MainHeader/>
     <b-container>
         <RouterView/>
     </b-container>
---
+++ frontend/src/containers/MainHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-container>
+            <b-navbar-brand to="/">Notes</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item :to="{name: 'home'}">Home</b-nav-item>
+                    <b-nav-item v-if="!user" :to="{name: 'login'}">Вход</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item v-if="user">{{ user.email }}</b-nav-item>
+            </b-navbar-nav>
+        </b-container>
+    </b-navbar>
+</template>
+
+<script>
+import {useAuthStore} from "@/stores/auth";
+import {mapState} from "pinia";
+
+export default {
+    name: "MainHeader",
+    computed: mapState(useAuthStore, ['user'])
+}
+</script>
+
+<style scoped>
+
+</style>
